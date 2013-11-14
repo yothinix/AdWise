009637a7cff9e8b5c46b760ff23d66644c48e623
Ratchanon  Jungsanguansit <iiiforceiii@gmail.com>
2013-11-14 16:33:29 +0700

    Initial Commit Force

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'force';
 $db['default']['database'] = 'project';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
