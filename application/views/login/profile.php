3f5bb175a64cbeede62329b6c7ecd00c598aa0de
Patthira Khakhuntod <pattc_k@hotmail.com>
2013-11-20 14:48:26 +0700

    profile -> query ข้อมูลจาก db และทำการ update ได้

--- application/views/login/profile.php
+++ application/views/login/profile.php
@@ -13,85 +13,90 @@
                 $pro = array(
                     'class' => 'form-horizontal'
                 );
-                echo form_open('user/profile',$pro);
+
+                foreach($profile as $row)
+                {
+
+                echo form_open('user/update');
                 ?>
+
 
                 <div class="control-group">
                     <label class="control-label" for="inputName"> Name </label>
                     <div class="controls">
-                        <input type="text" id="name" placeholder="Name" value="<?php echo set_value('name'); ?>">
+                        <input type="text" name="name" value="<?php echo $row->Name ?>">
                     </div>
                 </div>
+
 
                 <div class="control-group">
                     <label class="control-label" for="inputLastname"> Lastname </label>
                     <div class="controls">
-                        <input type="text" id="lastname" placeholder="Lastname" value="<?php echo set_value('lastname'); ?>">
+                        <input type="text" name="lastname" value="<?php echo $row->Lastname ?>">
                     </div>
                 </div>
 
                 <div class="control-group">
                     <label class="control-label" for="inputGender"> Gender </label>
                     <div class="controls">
-                        <input type="radio" name="gender" value="male" <?php echo set_radio('gender', 'male', TRUE); ?> /> Male
-                        <input type="radio" name="gender" value="female" <?php echo set_radio('gender', 'female'); ?> /> Female
+                        <input type="radio" id="gender" value=""> Male
+                        <input type="radio" id="gender" value=""> Female
                     </div>
                 </div>
 
                 <div class="control-group">
                     <label class="control-label" for="inputBirthday"> Birthday </label>
                     <div class="controls">
-                    <input type="text" id="birthday" placeholder="Birthday" value="<?php echo set_value('birthday'); ?>">
+                        <div class='input-append date' id='datetimepicker1'>
+                            <input placeholder="Birthday" data-format='dd/MM/yyyy' type='text' />
+                         <span class='add-on'>
+                         <i data-date-icon='icon-calendar'>
+                         </i>
+                         </span>
+                        </div>
                     </div>
                 </div>
+                    <script type='text/javascript'>
+                        $(function() {
+                            $('#datetimepicker1').datetimepicker({
+                                language: 'pt-BR'
+                                });
+                            });
+                    </script>
 
                 <div class="control-group">
                     <label class="control-label" for="inputPhone"> Phone </label>
                     <div class="controls">
-                        <input type="text" id="phone" placeholder="Phone" value="<?php echo set_value('phone'); ?>">
+                        <input type="text" name="phone" value="<?php echo $row->Phone ?>">
                     </div>
                 </div>
 
                 <div class="control-group">
                     <label class="control-label" for="inputEmail"> Email </label>
                     <div class="controls">
-                        <input type="text" id="email" placeholder="Email" value="<?php echo set_value('email'); ?>">
+                        <input type="text" name="email" value="<?php echo $row->Email ?>">
                      </div>
                 </div>
 
+
                 <div class="control-group">
                     <div class="controls">
-                        <button type="submit" value="save" class="btn btn-success">Save</button>
-                        <button type="clear" value="clear" class="btn btn-danger">Clear</button>
+                        <button type="submit" class="btn btn-success">Save</button>
+                        <?php } //ตัวปิด ?>
+                        <?php echo form_close(); ?>
                     </div>
                 </div>
 
 
-                <div class="control-group">
-                    <label class="control-label" for="inputNewpassword"> New Password </label>
-                    <div class="controls">
-                        <input type="text" id="newpassword" placeholder="New Password" value="<?php echo set_value('newpassword'); ?>">
-                    </div>
-                </div>
 
-                <div class="control-group">
-                    <label class="control-label" for="inputConfirmpassword"> Confirm Password </label>
-                    <div class="controls">
-                        <input type="text" id="confirmpassword" placeholder="Confirm Password" value="<?php echo set_value('confirmpassword'); ?>">
-                    </div>
-                </div>
 
-                <div class="control-group">
-                    <div class="controls">
-                        <button type="submit" value="save" class="btn btn-success">Save</button>
-                        <button type="clear" value="clear" class="btn btn-danger">Clear</button>
-                    </div>
-                </div>
-
-                <?php echo form_close(); ?>
 
             </div><!--/span-->
         </div>
     </div>
 </div>
 </div>
+
+
+<script type="text/javascript" src="<?php echo base_url("/assets/js/bootstrap-datetimepicker.min.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("/assets/js/bootstrap-datetimepicker.pt-BR.js"); ?>"></script>
