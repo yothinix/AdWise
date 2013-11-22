6add082a15efa4e668c8045a778ef47834e63e1e
Yothin Muangsommuk <yothin@kmi.tl>
2013-11-22 22:14:05 +0700

    Compatibility w/ my environment (base_url, db)

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/adwise';
+$config['base_url']	= 'http://localhost/project';
 
 $config['server_root'] = $_SERVER['DOCUMENT_ROOT']; 
 /*
