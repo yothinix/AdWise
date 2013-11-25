dbe8451361ca571f8201f8adf39819805e0f11cf
Yothin Muangsommuk <yothin@kmi.tl>
2013-11-25 15:21:23 +0700

    Fixed Header Nav Link _Manage_user

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -39,6 +39,7 @@
 $asmlist = base_url("index.php/assessment/asmlist");
 $profile = base_url("index.php/user/profile");
 $result = base_url("index.php/assessment/result");
+$manage_user = base_url("index.php/manage/manage_user");
 ?>
 
 <div class="navbar navbar-inverse navbar-fixed-top">
@@ -98,7 +99,8 @@ $result = base_url("index.php/assessment/result");
                             <li><a href="#"><i class="icon-random"></i>Manage Answer Group</a></li>
                             <li><a href="#"><i class="icon-tags"></i>Manage Occupation</a></li>
                             <li><a href="#"><i class="icon-tags"></i>Manage Academic</a></li>
-                            <li><a href="#"><i class="icon-user"></i>Manage User</a></li>
+                            <li <?php if($main_content == 'manage_user'){ echo "class=\"active\""; } ?>>
+                                <a href="<?php echo $manage_user; ?>"><i class="icon-user"></i>Manage User</a></li>
                             <li><a href="#"><i class="icon-home"></i>Analytics</a></li>
                     <?php
                         }
