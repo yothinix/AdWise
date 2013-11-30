c128f8b34426856ac131735589df17814d5febe0
Yothin Muangsommuk <yothin@kmi.tl>
2013-12-01 04:01:38 +0700

    [fix] dashboard: base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/project';
+$config['base_url']	= 'http://localhost/adwise';
 
 $config['server_root'] = $_SERVER['DOCUMENT_ROOT']; 
 /*
