9dcad522f275bc04509f58769519d5f6fde0b894
Yothin Muangsommuk <yothin@kmi.tl>
2013-12-02 23:48:12 +0700

    [11-1-1] -Fix $js calling footer กูไม่น่าแก้เล้ยยยยยยยย

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -6,19 +6,19 @@
 <!-- Le javascript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="<?php echo base_url("/assets/js/jquery.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-transition.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-alert.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-modal.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-dropdown.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-scrollspy.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-tab.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-tooltip.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-popover.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-button.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-collapse.js"); ?>"></script>
-<script src="<?php echo base_url("assets/js/bootstrap-carousel.js"); ?>"></script>
-<script src="<?php echo base_url("/assets/js/bootstrap-typeahead.js");?>"></script>
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/bootstrap-transition.js"></script>
+<script src="assets/js/bootstrap-alert.js"></script>
+<script src="assets/js/bootstrap-modal.js"></script>
+<script src="assets/js/bootstrap-dropdown.js"></script>
+<script src="assets/js/bootstrap-scrollspy.js"></script>
+<script src="assets/js/bootstrap-tab.js"></script>
+<script src="assets/js/bootstrap-tooltip.js"></script>
+<script src="assets/js/bootstrap-popover.js"></script>
+<script src="assets/js/bootstrap-button.js"></script>
+<script src="assets/js/bootstrap-collapse.js"></script>
+<script src="assets/js/bootstrap-carousel.js"></script>
+<script src="assets/js/bootstrap-typeahead.js"></script>
 
 </body>
 </html>
