b35513cdc90d1b1bc5a20f0588c5b2cc3d60c4c7
Ratchanon  Jungsanguansit <iiiforceiii@gmail.com>
2013-12-04 16:35:09 +0700

    [10-1-0] สร้างหน้า view ของ manage occupation data

--- application/controllers/manage.php
+++ application/controllers/manage.php
@@ -32,42 +32,15 @@ class Manage extends CI_Controller{
         $this->load->view('includes/template', $data);
 
     }
-    /*public function del($id)
+
+    function manage_occupation_data()
     {
-        $this->db->delete("login",array('id'=>$id));
-        redirect("member","refresh");
-        exit();
+        $data = array(
+            'main_content' => 'manage_occupation_data',
+        );
+        $this->load->view('manage occupation data', $data);
+
     }
-    public function edit($id)
-    {
-        if($this->input->post("btsave")!=null)
-        {
-
-            $ar=array(
-
-                'ID'=>$this->input->post("ID"),
-                'Name'=>$this->input->post("Name"),
-                'Lastname'=>$this->input->post("Lastname")
-            );
-
-            $this->db->where('id',$id);
-            $this->db->update("login",$ar);
-            redirect("member","refresh");
-            exit();
-        }
-        <div class="modal hide fade">
-        <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h3>Modal header</h3>
-        </div>
-        <div class="modal-body">
-        <p>One fine body…</p>
-        </div>
-        <div class="modal-footer">
-        <a href="#" class="btn">Close</a>
-        <a href="#" class="btn btn-primary">Save changes</a>
-        </div>
-        </div> */
 
 }
 ?>
