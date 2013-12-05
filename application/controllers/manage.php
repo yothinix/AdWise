31b2719ea71548eb8b0a4e82bdd8e1260c5f966a
Yothin Muangsommuk <yothin@kmi.tl>
2013-12-06 00:35:31 +0700

    Merge branch 'feature/Manage_Academic_Data' into develop
    Conflicts:
    	application/config/config.php
    	application/controllers/manage.php

--- application/controllers/manage.php
+++ application/controllers/manage.php
@@ -6,6 +6,7 @@ class Manage extends CI_Controller{
         $this->load->model('User_model');
         $this->load->model('Assessment_model');
         $this->load->model('Manage_assessment_type');
+        $this->load->model('Manage_academic');
     }
 
     function index()
@@ -70,6 +71,44 @@ class Manage extends CI_Controller{
         <a href="#" class="btn btn-primary">Save changes</a>
         </div>
         </div> */
+
+    function manage_academic()
+    {
+        $user = $this->Manage_academic->academic();
+        $data = array(
+            'main_content' => 'manage_academic',
+            'manage_academic' => $user
+        );
+        $this->load->view('includes/template', $data);
+    }
+
+    function del_academic($Academic_id)
+    {
+        $this->db->delete('academic',array('Academic_id' => $Academic_id));
+        $this->manage_academic();
+    }
+
+    function create_academic()
+    {
+        $data = array(
+            'name'=>$this->input->post('name'),
+            'detail'=>$this->input->post('detail'),
+            'tag'=>($this->input->post('tag'))
+        );
+        $this->Manage_academic->create_academic($data);
+        $this->manage_academic();
+    }
+
+    function update($academic_id)
+    {
+        $data = array(
+            'name'=>$this->input->post('name'),
+            'detail'=>$this->input->post('detail'),
+            'tag'=>($this->input->post('tag'))
+        );
+        $this->Manage_academic->update($academic_id ,$data);
+        $this->manage_academic();
+    }
 
     function manage_assessment()
     {
