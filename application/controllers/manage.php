2ddd3b51c05c191ac3a5548a0847c56ab0c29078
Yothin Muangsommuk <yothin@kmi.tl>
2013-12-06 00:55:50 +0700

    Merge branch 'feature/Manage_result' into develop
    Conflicts:
    	application/config/config.php
    	application/controllers/manage.php

--- application/controllers/manage.php
+++ application/controllers/manage.php
@@ -8,6 +8,8 @@ class Manage extends CI_Controller{
         $this->load->model('Manage_assessment_type');
         $this->load->model('Manage_academic');
         $this->load->model('Manage_answer_group');
+        $this->load->model('Manage_occupation');
+        $this->load->model('Manage_result_data');
     }
 
     function index()
@@ -34,44 +36,13 @@ class Manage extends CI_Controller{
             'manage_user' => $user
         );
         $this->load->view('includes/template', $data);
-
     }
-    /*public function del($id)
+
+    function delete_user($userID)
     {
-        $this->db->delete("login",array('id'=>$id));
-        redirect("member","refresh");
-        exit();
+        $this->db->delete('user', array('ID' => $userID));
+        $this->manage_user();
     }
-    public function edit($id)
-    {
-        if($this->input->post("btsave")!=null)
-        {
-
-            $ar=array(
-
-                'ID'=>$this->input->post("ID"),
-                'Name'=>$this->input->post("Name"),
-                'Lastname'=>$this->input->post("Lastname")
-            );
-
-            $this->db->where('id',$id);
-            $this->db->update("login",$ar);
-            redirect("member","refresh");
-            exit();
-        }
-        <div class="modal hide fade">
-        <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h3>Modal header</h3>
-        </div>
-        <div class="modal-body">
-        <p>One fine body…</p>
-        </div>
-        <div class="modal-footer">
-        <a href="#" class="btn">Close</a>
-        <a href="#" class="btn btn-primary">Save changes</a>
-        </div>
-        </div> */
 
     function manage_academic()
     {
@@ -181,6 +152,97 @@ class Manage extends CI_Controller{
 
         //then load manage assessment page again
         $this->manage_answer_group();
+    }
+
+    /*Manage_result function*/
+
+    function  manage_result()
+    {
+        $this->load->model('Manage_result_data');
+        $user = $this->Manage_result_data->get_manage_result();
+        $data = array(
+            'main_content' => 'manage_result',
+            'manage_result' => $user
+        );
+        $this->load->view('includes/template', $data);
+    }
+
+    function create_result()
+    {
+        $data = array(
+            'name'=>$this->input->post('name'),
+            'detail'=>$this->input->post('detail')
+
+        );
+        $this->Manage_result_data->create_result($data);
+        $this->manage_result();
+    }
+
+    function update_result($ResultID)
+    {
+        $data = array(
+            'Name'=>$this->input->post('name'),
+            'Detail'=>$this->input->post('detail')
+        );
+        $this->Manage_result_data->update_result($ResultID ,$data);
+        $this->manage_result();
+    }
+
+    function delete_result($ResultID)
+    {
+        $this->db->delete('result', array('ResultID' => $ResultID));
+        $this->manage_result();
+    }
+
+    /*Manage_Occupation function*/
+
+    function manage_occupation_data()
+    {
+        $data = array(
+            'main_content' => 'manage_occupation_data',
+        );
+        $this->load->view('manage occupation data', $data);
+    }
+
+    function  manage_occupation()
+    {
+        $this->load->model('Manage_occupation');
+        $user = $this->Manage_occupation->get_manage_occupation();
+        $data = array(
+            'main_content' => 'manage_occupation',
+            'manage_occupation' => $user
+        );
+        $this->load->view('includes/template', $data);
+    }
+
+    function delete_occupation($Occupation_id)
+    {
+        $this->db->delete('occupation', array('Occupation_id' => $Occupation_id));
+        $this->manage_occupation();
+    }
+
+    function create_occupation()
+    {
+        $data = array(
+            'name'=>$this->input->post('name'),
+            'detail'=>$this->input->post('detail'),
+            'tag'=>($this->input->post('tag')),
+            //'academic_id'=>($this->input->post('academic'))//
+        );
+        $this->Manage_occupation->create_occupation($data);
+        $this->manage_occupation();
+    }
+
+    function update($occupation_id)
+    {
+        $data = array(
+            'name'=>$this->input->post('name'),
+            'detail'=>$this->input->post('detail'),
+            'tag'=>($this->input->post('tag'))
+            //'academic_id'=>($this->input->post('academic'))//
+        );
+        $this->Manage_occupation->update($occupation_id ,$data);
+        $this->manage_occupation();
     }
 
 
