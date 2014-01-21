807155a5fccc0105dd70fceab451dd91458959a0
Yothin Muangsommuk <yothin@kmi.tl>
2014-01-22 01:32:53 +0700

    Setting up Linux development variable
    Signed-off-by: Yothin Muangsommuk <yothin@kmi.tl>

--- assets/js/tag_typehead.js
+++ assets/js/tag_typehead.js
@@ -1,7 +1,7 @@
 $('.example_typeahead > > input').tagsinput({
   typeahead: {
     source: function(query) {
-      return $.getJSON('http://localhost/project/assets/tags.json');
+      return $.getJSON('http://localhost/AdWise/assets/tags.json');
     }
   }
 });
