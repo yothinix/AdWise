086fd9705d9eb7bc42ee78888665afb8cd6a583e
Yothin Muangsommuk <yothin@kmi.tl>
2014-02-02 14:49:51 +0700

    [11] Add How to Button in create_assessment/result_condition form

--- application/views/createAsm/result_condition.php
+++ application/views/createAsm/result_condition.php
@@ -49,8 +49,8 @@ $next = "review_condition";
 ?>
     <textarea type="text" rows="10" name="result_exp" class="input-block-level" placeholder ="Result Expression"><?php echo $this->session->userdata('Expression'); ?></textarea>
         <div style="text-align: center; margin-top: 20px">
-            <input type="submit" name="check_exp" class="btn btn-warning" value="Check Expression" />
-            <input type="submit" name="add_exp" class="btn btn-success" value="Add Expression" />
+            <a href="#re_desc" role="button" class="btn" data-toggle="modal">How To</a>
+            <input type="submit" name="add_exp" class="btn btn-primary" value="Add Expression" />
         </div>
 <?php
     echo form_close();
@@ -58,6 +58,17 @@ $next = "review_condition";
     </div>
 <hr>
 
-
-
-
+<!-- Modal -->
+<div id="re_desc" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel">How To</h3>
+  </div>
+  <div class="modal-body">
+    <p> <h5>Result Expression</h5> เป็นรูปแบบของภาษาที่ทีม AdWise ได้พัฒนาขึ้นโดยอาศัยความสัมพันธ์ของตำแหน่งของกลุ่มคำตอบ (Answer group) เทียบกับรูปแบบผลลัพธ์ (Result) ที่มีอยู่ในระบบ การประมวลผลในแต่ละตำแหน่งของผลลัพธ์จะคิดเป็น 1 คำสั่ง (expression) ซึ่งแต่ละคำสั่งจะถูกคั่นด้วยเครื่องหมาย semicolon (;) โดยในการประมวลผลคำตอบ Result Expression จะเลือกกลุ่มคำตอบในคำสั่งที่มีค่าผลรวมที่ผู้ใช้ทำการตอบสูงสุดเป็นตัวแทนกลุ่มในการหาผลลัพธ์ต่อไป 
+    </p>
+    <h5>Example Usage</h5>
+    <pre>pair(1) = {awg(3),awg(2),awg(1)};</pre>
+    <p>จากตัวอย่าง หมายความว่า ตำแหน่งแรกของผลลัพธ์สามารถเกิดขึ้นได้จากกลุ่มคำตอบที่มี id = 3,2,1 ได้</p>
+  </div>
+</div>
