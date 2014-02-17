d1262f562c743018fcdb4b87cd45f30ae5191b01
Yothin Muangsommuk <yothin@kmi.tl>
2014-02-17 15:03:32 +0700

    Interface Apriori algorithm + enhancement

---
+++ application/controllers/apriori.php
@@ -0,0 +1,19 @@
+<?php
+
+class Apriori extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function Apriori($Dataset)
+    {
+        //this function get dataset from sets of | result | occupation | tags(i) |
+        //using minimum support = 2 and target support = 3 as a variable
+        //Implement base on Apriori algorithm with enchancement in Dynamic minimum support
+        //and targetsets as a result to compare with TID or occupationID 
+
+    }
+
+}
