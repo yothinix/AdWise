5af039b5afcd22d28b8b7f70ccd64a04301a3fad
Ratchanon Jungsanguansit <iiiforceiii@gmail.com>
2014-03-04 16:07:05 +0700

    Analytics graph complete

--- application/views/analytics.php
+++ application/views/analytics.php
@@ -63,8 +63,18 @@
         height: 400px;
         border: 2px;
     }
+    #Male_discrete svg{
+        height: 500px;
+        min-width: 100px;
+        min-height: 100px;
 
+    }
+    #Female_discrete svg{
+        height: 500px;
+        min-width: 100px;
+        min-height: 100px;
 
+    }
 </style>
 <link href="<?php echo base_url("/assets/js/pie/src/nv.d3.css"); ?>" rel="stylesheet" type="text/css">
 <h2 style="margin-top: -30px">Analytics</h2>
@@ -84,10 +94,11 @@ echo form_open($controller);
                     echo "<option value='".$row["AssessmentID"]."' >".$row["Name"]."</option>";
                 }
                 ?>
+
             </select>
          </td>
          <td>
-             <select name="graphID">
+             <select id="graphID" name="graphIDD">
                  <option value="" style="display:none;"><------- Select Graph -------></option>
                  <?php
                  $graph = $this->Analytics_model->graph();
@@ -97,7 +108,7 @@ echo form_open($controller);
                     <option value="<?php echo $gh->graphID ?>" > <?php echo $gh->name ?> </option>
                  <?php }
                  ?>
-
+                 <option value="<?php echo $gh->graphID ?>" > <?php echo $gh->name ?> </option>
              </select>
          </td>
          <td style=" padding-bottom:13px ">
@@ -113,12 +124,12 @@ echo form_close();
 <table style="margin-top: 80px;" >
     <tr>
         <td>
-<h2 style="position: absolute;">Male</h2>
-<svg id="Male" class="mypiechart"></svg>
+<h2 id="Male_h" style="position: absolute; visibility: hidden;">Male</h2>
+            <svg id="Male" class="mypiechart" style="margin-top: 50"></svg>
         </td>
         <td>
-<h2 style="position: absolute;" >Female</h2>
-<svg id="Female" class="mypiechart"></svg>
+<h2 id="Female_h" style="position: absolute; visibility: hidden;" >Female</h2>
+            <svg id="Female" class="mypiechart" style="margin-top: 50"></svg>
         </td>
     </tr>
 </table>
@@ -130,44 +141,129 @@ echo form_close();
 <script type="text/javascript" src="<?php echo base_url("/assets/js/pie/src/models/pie.js"); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url("/assets/js/pie/src/models/pieChart.js"); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url("/assets/js/pie/src/utils.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("/assets/js/discrete/nv.d3.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("/assets/js/discrete/lib/d3.v3.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("/assets/js/discrete/src/tooltip.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("/assets/js/discrete/src/utils.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("/assets/js/discrete/src/models/axis.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("/assets/js/discrete/src/models/discreteBar.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("/assets/js/discrete/src/models/discreteBarChart.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("/assets/js/horizontal/lib/d3.v2.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("/assets/js/horizontal/lib/horizon.js"); ?>"></script>
+
 
 <script>
+
     //This is JSON data via JavaScript Template
 //Need to json_encode the php array before sending to relate function
     var male = <?php echo $result_male; ?>;
     var female = <?php echo $result_female; ?>;
-    nv.addGraph(function() {
-        var width = 430,
-            height = 430;
+    var data = <?php echo json_encode($check); ?>;
+    var graph = 0;
+    if(data=='data'){
+        graph = <?php echo $graph_Select; ?>;
+        var header_male = document.getElementById("Male_h");
+        var header_female = document.getElementById("Female_h");
 
-        var chart = nv.models.pieChart()
-            .x(function(d) { return d.key })
-            .y(function(d) { return d.y })
-            .color(d3.scale.category10().range())
-            .width(width)
-            .height(height);
+        header_male.style.visibility='visible';
+        header_female.style.visibility='visible';
 
-        d3.select("#Male")
-            .datum(male)
-            .transition().duration(1200)
-            .attr('width', width)
-            .attr('height', height)
-            .call(chart);
+    }
 
-        d3.select("#Female")
-            .datum(female)
-            .transition().duration(1200)
-            .attr('width', width)
-            .attr('height', height)
-            .call(chart);
+    if(graph == 1){
+        ///alert("1");
+        nv.addGraph(function() {
+
+            var width = 430,
+                height = 430;
+
+            var chart = nv.models.pieChart()
+                .x(function(d) { return d.label })
+                .y(function(d) { return d.value })
+                .color(d3.scale.category10().range())
+                .width(width)
+                .height(height);
+
+            d3.select("#Male")
+                .datum(male)
+                .transition().duration(1200)
+                .attr('width', width)
+                .attr('height', height)
+                .call(chart);
+
+            d3.select("#Female")
+                .datum(female)
+                .transition().duration(1200)
+                .attr('width', width)
+                .attr('height', height)
+                .call(chart);
 
 
-        chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+            chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
 
-        return chart;
-    });
+            return chart;
+        });
+    }else if(graph == 2){
+        //alert("2");
 
+        nv.addGraph(function() {
+            var chart = nv.models.discreteBarChart()
+                    .x(function(d) { return d.label })
+                    .y(function(d) { return d.value })
+                    .staggerLabels(true)
+                    //.staggerLabels(historicalBarChart[0].values.length > 8)
+                    .tooltips(false)
+                    .showValues(true)
+                    .transitionDuration(250)
+                ;
 
+            d3.select("#Male")
+                .datum(return_data(male))
+                .call(chart);
+
+            d3.select("#Female")
+                .datum(return_data(female))
+                .call(chart);
+
+            nv.utils.windowResize(chart.update);
+
+            return chart;
+        });
+    }else if (graph == 4){
+        nv.addGraph(function() {
+            var chart = nv.models.discreteBarChart()
+                    .x(function(d) { return d.label })
+                    .y(function(d) { return d.value })
+                    .staggerLabels(true)
+                    //.staggerLabels(historicalBarChart[0].values.length > 8)
+                    .tooltips(false)
+                    .showValues(true)
+                    .transitionDuration(250)
+                ;
+
+            d3.select("#Male")
+                .datum(male)
+                .call(chart);
+
+            d3.select("#Feale")
+                .datum(female)
+                .call(chart);
+
+            nv.utils.windowResize(chart.update);
+
+            return chart;
+        });
+    }
+
+    function return_data(data){
+
+        return  [
+            {
+                key: "Cumulative Return",
+                values: data
+            }
+        ];
+    }
 
 
 </script>
